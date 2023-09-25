6dce1fb273e959071463970a3fac316f82faafb4
Rele <90725309+Phenix34@users.noreply.github.com>
2023-09-25 10:20:33 -0400

    cositas necesarias

---
+++ DAL/DataContext/WebApiContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LamasKpita.DataContext;
+
+public partial class WebApiContext : DbContext
+{
+    public WebApiContext()
+    {
+    }
+
+    public WebApiContext(DbContextOptions<WebApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=EVA02; Database=WebAPI; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Persona__3214EC271A6AE1F2");
+
+            entity.ToTable("Persona");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido).HasMaxLength(50);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Entities/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LamasKpita.DataContext;
+
+public partial class Persona
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public int? Edad { get; set; }
+
+    public string? Email { get; set; }
+
+    public DateTime? FechaNacimiento { get; set; }
+}
